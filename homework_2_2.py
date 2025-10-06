25c677e6bc540017d6f871c99cb29153306d39c7
BigSmoke <farhsmachineone@gmail.com>
2025-10-06 18:40:37 +0300

    lesson 1

---
+++ homework_2_2.py
@@ -0,0 +1,131 @@
+# 1. Работа с целыми и вещественными числами
+# a = 8
+# b = 3
+# x = 2.5
+# y = -1.7
+# print(type(a))
+# print(type(b))
+# print(type(x))
+# print(type(y))
+
+# 2. Основные арифметические операции
+# num_1 = 10
+# num_2 = 3
+#
+# print("Сложение", num_1 + num_2)
+# print("Вычитание", num_1 - num_2)
+# print("Умножение", num_1 * num_2)
+# print("Деление", num_1 / num_2)
+# print("Целочисленное деление", num_1 // num_2)
+# print("Остаток от деления", num_1 % num_2)
+# print("Возведение в степень", num_1 ** num_2)
+
+# 3. Особенности работы с делением
+# a = 10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# a = -10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# a = 10
+# b = -3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# 4. Работа с приоритетом операторов
+
+# a = 5 + 3 * 2
+# print(a)
+#
+# b = (5 + 3) * 2
+# print(b)
+#
+# c = 10 / 2 ** 2
+# print(c)
+
+# 5. Использование сокращенных операторов
+# count = 10
+# count += 5
+# print(count)
+#
+# count -= 5
+# print(count)
+#
+# count *= 2
+# print(count)
+#
+# count /= 2
+# print(count)
+
+# 1. Создание строк
+# s1 = "Python"
+# s2 = "Програмирование"
+# print(s1)
+# print(s2)
+#
+# s3 = """Python Програмирование"""
+# print(s3)
+#
+# empty = ""
+# print(len(empty))
+
+# 2. Конкатенация строк
+# first_name = "Виталий"
+# last_name = "Полищук"
+#
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+# 3. Преобразование типов
+# s = "Возраст "
+# age = 25
+# all = s + str(age)
+# print(all)
+
+# # 4. Дублирование строк
+# a = "xa " * 5
+# print(a)
+
+# a = "xa" * 2.5
+# print(a)
+# TypeError: can't multiply sequence by non-int of type 'float'
+
+#
+# 5. Длина строки
+# text = "Hello World!"
+# print(len(text))
+#
+# empty = ""
+# print(len(empty))
+
+# 6. Проверка вхождения подстроки
+
+# sentence = "Я изучаю Python"
+# python = "Python" in sentence
+# print(python)
+# print("Java" not in sentence)
+
+# 7. Сравнение строк
+# a = "apple"
+# b = "banana"
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+
+# # 8. Код символов
+# cod = ord("A")
+# print(cod)
+# cod_2 = ord("a")
+# print(cod_2)
+# cod_3 = ord("Я")
+# print(cod_3)
