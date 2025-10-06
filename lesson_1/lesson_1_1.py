aac94374c71e2a5cceb417573ba0eee288327b82
BigSmoke <farhsmachineone@gmail.com>
2025-10-06 15:03:51 +0300

    lesson 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,34 @@
-print("Hello World")
+# a = 7
+# str_1 = "test"
+# b = 8.34
+# b = a
+# a = 8
+# print(b)
+from itertools import count
+
+from pyexpat.errors import messages
+
+# a = b = c = 0
+# a, b, c = 1, 2, 3
+# print(a)
+# print(b)
+# print(c)
+# a, b = 8, 10
+# a, b = b, a
+# print(a)
+# print(b)
+
+# print(type(a))
+# print(type(str_1))
+# print(type(b))
+#
+# message = "Сообщение"
+# count = 0
+# Count = 0
+
+a = c = 4
+b, d = "String", "two string"
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
